b729b55d735171edfbd9bec70f9f1c567b500408
honeion <47212652+honeion@users.noreply.github.com>
2022-09-06 21:54:57 +0900

    Delete back-end directory

--- back-end/partymanagement/src/test/java/partymanagement/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package partymanagement.common;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import partymanagement.PartyManagementApplication;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { PartyManagementApplication.class })
-public class CucumberSpingConfiguration {}
